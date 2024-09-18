61344271889fd9705843494815cad1a83dca256c
MarleySilva001 <marley_c_silva@estudante.sesisenai.org.br>
2024-09-18 00:15:26 -0300

    rotas 1.0

--- app/buttonRouter.jsx
+++ app/buttonRouter.jsx
@@ -1,11 +1,29 @@
-const ButtonR = ({href, Text}) => {
+import { Link } from "expo-router";
+import { Text, Pressable, StyleSheet } from "react-native";
+
+const ButtonR = ({href, text}) => {
     return(
         <Link href={href}>
-                <Pressable>
-                    <Text>{Text}</Text>
+                <Pressable style={styles.button}>
+                    <Text style={styles.text}>{text}</Text>
                     </Pressable>
                 </Link>
     )
 }
+
+const styles = StyleSheet.create({
+    button : {
+        color: 'white',
+        backgroundColor: '#00BF66',
+        width: 300,
+        borderRadius: 10,
+        paddingHorizontal: 10,
+        paddingVertical: 6,
+        textAlign: 'center'
+    },
+    text : {
+        width: '100%'
+    }
+})
 
 export default ButtonR
--- app/index.jsx
+++ app/index.jsx
@@ -1,36 +1,38 @@
-import { Link } from "expo-router";
-import { View, Text, TextInput, Pressable, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import ButtonR from "./buttonRouter";
 
 const Rotas = () => {
     return (
         <View style={styles.container}>
-            <View style={styles.container}>
-                <Link href="/Santander">
-                <Pressable>
-                    <Text>Banco Santander</Text>
-                    </Pressable>
-                </Link>
-                <Link href="/calculadora 2.0">
-                <Pressable>
-                    <Text>calculadora 2</Text>
-                    </Pressable>
-                </Link>
-                <Link href="/calculadora simples">
-                <Pressable>
-                    <Text>calculadora 1</Text>
-                    </Pressable>
-                </Link>
-                <Link href="/pokemon">
-                <Pressable>
-                    <Text>pokemon</Text>
-                    </Pressable>
-                </Link>
-                <Link href="/TaskHub">
-                <Pressable>
-                    <Text>TaskHub</Text>
-                    </Pressable>
-                </Link>
-                
+            <View style={styles.liRotas}>
+               <ButtonR 
+               href={"/Santander"}
+               text={"banco santander"}
+               />
+               <ButtonR 
+               href={"/calculadora 2.0"}
+               text={"calculadora 2"}
+               />
+               <ButtonR 
+               href={"/calculadora simples"}
+               text={"calculadora 1"}
+               />
+               <ButtonR 
+               href={"/pokemon"}
+               text={"pokemon"}
+               />
+               <ButtonR 
+               href={"/TaskHub"}
+               text={"Task Hub"}
+               />
+               <ButtonR 
+               href={"/lista-tarefa"}
+               text={"Lista Tarefa"}
+               />
+               <ButtonR 
+               href={"/splashScreen"}
+               text={"Splash Screen"}
+               />
             </View>
         </View>
     )
@@ -38,10 +40,19 @@ const Rotas = () => {
 
 const styles = StyleSheet.create({
     container: {
-        display: 1,
+        flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-    }
-})
+        padding: 16,
+    },
+    liRotas: {
+        flex: 1,
+        width: '100%',
+        gap: 6,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingVertical: 10,
+    },
+});
 
 export default Rotas;
