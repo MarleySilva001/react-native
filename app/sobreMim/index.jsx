52d57f9798173ce4ffd4d18b2bc5150a19f7d70f
MarleySilva001 <marley_c_silva@estudante.sesisenai.org.br>
2024-09-18 20:19:48 -0300

    sonreMim 0.3

--- app/sobreMim/index.jsx
+++ app/sobreMim/index.jsx
@@ -1,24 +1,63 @@
-import { ScrollView, StyleSheet, Text, View } from "react-native"
+import { ScrollView, StyleSheet, Text, View, Pressable, Image, FlatList } from "react-native"
 import ButtonSM from "./buttonSM"
+import { Link } from "expo-router"
 
 const sobreMim = () => {
+    const data = [
+        {
+            id: '1',
+            nome: 'Desenhos',
+            href: 'sobreMim/desenho'
+        },
+        {
+            id: '2',
+            nome: 'Musicas',
+            href: 'sobreMim/musica'
+        }
+    ]
     return(
+        <View style={styles.container}>
+            <Text>HEADER</Text>
+            <Image 
+            style={styles.foto}
+            source={require('../../assets/images/rio2016.png')}
+            />
+            <Text style={styles.h1}>Bem-Vindo(a) ao meu App</Text>
+            <Text style={styles.p}>Olá sou um aluno da escola Sesi, estou cursando o terceiro ano do Ensino Médio integrado com o curso de Análise e Desenvolvimento de Sistemas. Nesse App pretendo mostra algumas coisas que eu gosto.</Text>
         <View>
-            <Text>sobreMim</Text>
-            <ButtonSM 
-            href={"/musica"}
-            text={"Albuns favoritos"}
+            <FlatList 
+            data={data}
+            keyExtractor={(item) => item.id}
+            renderItem={({item}) =>(
+                <ButtonSM 
+                href={item.href}
+                text={item.nome}
+                />
+            )}
             />
-            <ButtonSM 
-            href={"/sobreMim/2.desenho"}
-            text={"Melhores desenhos"}
-            />
+        </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+container:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+},
+foto:{
+    width: 200,
+    height: 200,
+    borderRadius: 50,
+},
+h1:{
+    fontWeight: 'bold',
+    fontSize: 34
+},
+p:{
+    width:350
+}
 })
 
 export default sobreMim
