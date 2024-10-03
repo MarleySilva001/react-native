595ca57d6e85063800af25674d6cbea8fa57ec30
MarleySilva001 <129191771+MarleySilva001@users.noreply.github.com>
2024-10-03 22:04:51 +0000

    routes 0.8

--- app/index.jsx
+++ app/index.jsx
@@ -6,23 +6,23 @@ import { Link } from "expo-router";
 const Rotas = () => {
 
     const data = [
-        { id: '1', href: "/Santander", text: "Santander", img: '../assets/images/marley.jpg' },
+        { id: '1', href: "/Santander", text: "Santander", img: '../assets/images/santander.png' },
         { id: '2', href: "/calculadora 2.0", text: "calculadora 2", img: '../assets/images/marley.jpg' },
         { id: '3', href: "/calculadora simples", text: "calculadora 1", img: '../assets/images/marley.jpg' },
-        { id: '4', href: "/pokemon", text: "pokemon" , img: '../assets/images/marley.jpg'},
-        { id: '5', href: "/TaskHub", text: "Task Hub", img: '../assets/images/marley.jpg' },
-        { id: '6', href: "/lista-tarefa", text: "Lista Tarefa", img: '../assets/images/marley.jpg' },
+        { id: '4', href: "/pokemon", text: "pokemon" , img: '../assets/images/pokeball.png'},
+        { id: '5', href: "/TaskHub", text: "Task Hub", img: '../assets/images/taskhub.png' },
+        { id: '6', href: "/lista-tarefa", text: "Lista Tarefa", img: '../assets/images/listatarefa.png' },
         { id: '7', href: "/splashScreen", text: "Splash Screen", img: '../assets/images/marley.jpg' },
         { id: '8', href: "/sobreMim", text: "Sobre Mim", img: '../assets/images/marley.jpg' },
-        { id: '9', href: "/iFome", text: "iFome", img: '../assets/images/marley.jpg' },
+        { id: '9', href: "/iFome", text: "iFome", img: '../assets/images/ifome.png' },
         { id: '10', href: "/ImagePicker", text: "image Picker", img: '../assets/images/marley.jpg' },
-        { id: '11', href: "/camera", text: "camera", img: '../assets/images/marley.jpg' },
+        { id: '11', href: "/camera", text: "camera", img: '../assets/images/camera.png' },
     ];
 
     return (
         <View style={styles.container}>
              <LinearGradient
-            colors={['white', 'aquamarine']}
+            colors={['#E9E9E9', 'aquamarine']}
             style={styles.background}
         />
             <View style={styles.liRotas}>
@@ -56,10 +56,9 @@ const styles = StyleSheet.create({
     liRotas: {
         flex: 1,
         width: '100%',
-        gap: 6,
         justifyContent: 'space-between',
         alignItems: 'center',
-        paddingVertical: 10,
+        paddingVertical: 14,
     },
 });
 
